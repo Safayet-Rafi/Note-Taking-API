a4be42d25f5ef78059a8cd90a08a630169ac02f1
Safayet-Rafi <safayetrafi461@gmail.com>
2025-08-05 23:33:39 +0600

    Implement logout functionality

--- src/main/java/com/example/note/controller/AuthController.java
+++ src/main/java/com/example/note/controller/AuthController.java
@@ -6,10 +6,8 @@ import com.example.note.dto.request.SignUpRequest;
 import com.example.note.dto.response.SignInResponse;
 import com.example.note.service.AuthService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -32,4 +30,11 @@ public class AuthController {
     public SignInResponse refreshAccessToken(@RequestBody RefreshTokenRequest request){
         return authService.refreshAccessToken(request);
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@RequestHeader("Authorization") String authHeader) {
+        authService.logout(authHeader);
+        return ResponseEntity.ok("Logged out successfully.");
+    }
+
 }
